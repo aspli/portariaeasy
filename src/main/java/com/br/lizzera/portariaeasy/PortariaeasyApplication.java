880c2d2553f3fb60c0241579ec39b54d0fdf3175
aspli <alexandre.xoquito@gmail.com>
2024-03-07 15:20:43 -0300

    Initial commit

---
+++ src/main/java/com/br/lizzera/portariaeasy/PortariaeasyApplication.java
@@ -0,0 +1,13 @@
+package com.br.lizzera.portariaeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortariaeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortariaeasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/lizzera/portariaeasy/PortariaeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.lizzera.portariaeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortariaeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
